721f269da925b7226b74b45d5244f85c1013b59b
galvarezleyton <gaalvarez0910@gmail.com>
2023-05-16 23:00:46 -0500

    feat: fix to pictures and prices

--- src/functions/items/items.service.ts
+++ src/functions/items/items.service.ts
@@ -7,15 +7,24 @@ async function getProductDetail(id: string) {
     axios.get(`${meliAPIURL}/items/${id}/description`),
   ]);
 
+  const picture =
+    itemRes.data.pictures && itemRes.data.pictures.length > 0
+      ? itemRes.data.pictures[0].url
+      : itemRes.data.thumbnail;
+
+  const priceParts = itemRes.data.price.toString().split(".");
+  const amount = Number(priceParts[0]);
+  const decimals = priceParts.length > 1 ? Number(priceParts[1]) : 0;
+
   const item = {
     id: itemRes.data.id,
     title: itemRes.data.title,
     price: {
       currency: itemRes.data.currency_id,
-      amount: Math.floor(itemRes.data.price),
-      decimals: itemRes.data.price - Math.floor(itemRes.data.price),
+      amount,
+      decimals,
     },
-    picture: itemRes.data.thumbnail,
+    picture,
     condition: itemRes.data.condition,
     free_shipping: itemRes.data.shipping.free_shipping,
     sold_quantity: itemRes.data.sold_quantity,
@@ -29,25 +38,33 @@ async function searchProducts(query, offset: number, limit: number) {
     `${meliAPIURL}/sites/MLA/search?q=${query}&offset=${offset}&limit=${limit}`
   );
 
-  const categories = searchRes.data.filters
-    ? searchRes.data.filters
-        .find((filter: any) => filter.id === "category")
-        ?.values.map((value: any) => value.path_from_root)
-        .flat(2)
-    : [];
+  const categories =
+    searchRes.data.filters && searchRes.data.filters.length > 0
+      ? searchRes.data.filters
+          .find((filter: any) => filter.id === "category")
+          ?.values.map((value: any) => value.path_from_root)
+          .flat(2)
+      : [];
+  console.log("si ingresa");
 
-  const items = searchRes.data.results.map((result: any) => ({
-    id: result.id,
-    title: result.title,
-    price: {
-      currency: result.currency_id,
-      amount: Math.floor(result.price),
-      decimals: result.price - Math.floor(result.price),
-    },
-    picture: result.thumbnail,
-    condition: result.condition,
-    free_shipping: result.shipping.free_shipping,
-  }));
+  const items = searchRes.data.results.map((result: any) => {
+    const priceParts = result.price.toString().split(".");
+    const amount = Number(priceParts[0]);
+    const decimals = priceParts.length > 1 ? Number(priceParts[1]) : 0;
+
+    return {
+      id: result.id,
+      title: result.title,
+      price: {
+        currency: result.currency_id,
+        amount,
+        decimals,
+      },
+      picture: result.thumbnail,
+      condition: result.condition,
+      free_shipping: result.shipping.free_shipping,
+    };
+  });
   return { author, categories, items };
 }
 
